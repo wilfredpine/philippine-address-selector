f94bafcc2a677ed4481b72bc19c8b4801db3b424
Wilfred Pine <only.master.red@gmail.com>
2021-11-02 08:30:44 +0800

    add hidden textbox

---
+++ ph-address-selector.js
@@ -0,0 +1,160 @@
+
+var my_handlers = {
+    // fill province
+    fill_provinces: function () {
+        //selected region
+        var region_code = $(this).val();
+
+        // set selected text to input
+        var region_text = $(this).find("option:selected").text();
+        let region_input = $('#region-text');
+        region_input.val(region_text);
+        //clear province & city & barangay input
+        $('#province-text').val('');
+        $('#city-text').val('');
+        $('#barangay-text').val('');
+
+        //province
+        let dropdown = $('#province');
+        dropdown.empty();
+        dropdown.append('<option selected="true" disabled>Choose State/Province</option>');
+        dropdown.prop('selectedIndex', 0);
+
+        //city
+        let city = $('#city');
+        city.empty();
+        city.append('<option selected="true" disabled></option>');
+        city.prop('selectedIndex', 0);
+
+        //barangay
+        let barangay = $('#barangay');
+        barangay.empty();
+        barangay.append('<option selected="true" disabled></option>');
+        barangay.prop('selectedIndex', 0);
+
+        // filter & fill
+        var url = 'ph-json/province.json';
+        $.getJSON(url, function (data) {
+            var result = data.filter(function (value) {
+                return value.region_code == region_code;
+            });
+
+            result.sort(function (a, b) {
+                return a.province_name.localeCompare(b.province_name);
+            });
+
+            $.each(result, function (key, entry) {
+                dropdown.append($('<option></option>').attr('value', entry.province_code).text(entry.province_name));
+            })
+
+        });
+    },
+    // fill city
+    fill_cities: function () {
+        //selected province
+        var province_code = $(this).val();
+
+        // set selected text to input
+        var province_text = $(this).find("option:selected").text();
+        let province_input = $('#province-text');
+        province_input.val(province_text);
+        //clear city & barangay input
+        $('#city-text').val('');
+        $('#barangay-text').val('');
+
+        //city
+        let dropdown = $('#city');
+        dropdown.empty();
+        dropdown.append('<option selected="true" disabled>Choose city/municipality</option>');
+        dropdown.prop('selectedIndex', 0);
+
+        //barangay
+        let barangay = $('#barangay');
+        barangay.empty();
+        barangay.append('<option selected="true" disabled></option>');
+        barangay.prop('selectedIndex', 0);
+
+        // filter & fill
+        var url = 'ph-json/city.json';
+        $.getJSON(url, function (data) {
+            var result = data.filter(function (value) {
+                return value.province_code == province_code;
+            });
+
+            result.sort(function (a, b) {
+                return a.city_name.localeCompare(b.city_name);
+            });
+
+            $.each(result, function (key, entry) {
+                dropdown.append($('<option></option>').attr('value', entry.city_code).text(entry.city_name));
+            })
+
+        });
+    },
+    // fill barangay
+    fill_barangays: function () {
+        // selected barangay
+        var city_code = $(this).val();
+
+        // set selected text to input
+        var city_text = $(this).find("option:selected").text();
+        let city_input = $('#city-text');
+        city_input.val(city_text);
+        //clear barangay input
+        $('#barangay-text').val('');
+
+        // barangay
+        let dropdown = $('#barangay');
+        dropdown.empty();
+        dropdown.append('<option selected="true" disabled>Choose barangay</option>');
+        dropdown.prop('selectedIndex', 0);
+
+        // filter & Fill
+        var url = 'ph-json/barangay.json';
+        $.getJSON(url, function (data) {
+            var result = data.filter(function (value) {
+                return value.city_code == city_code;
+            });
+
+            result.sort(function (a, b) {
+                return a.brgy_name.localeCompare(b.brgy_name);
+            });
+
+            $.each(result, function (key, entry) {
+                dropdown.append($('<option></option>').attr('value', entry.brgy_code).text(entry.brgy_name));
+            })
+
+        });
+    },
+
+    onchange_barangay: function () {
+        // set selected text to input
+        var barangay_text = $(this).find("option:selected").text();
+        let barangay_input = $('#barangay-text');
+        barangay_input.val(barangay_text);
+    },
+
+};
+
+
+$(function () {
+    // events
+    $('#region').on('change', my_handlers.fill_provinces);
+    $('#province').on('change', my_handlers.fill_cities);
+    $('#city').on('change', my_handlers.fill_barangays);
+    $('#barangay').on('change', my_handlers.onchange_barangay);
+
+    // load region
+    let dropdown = $('#region');
+    dropdown.empty();
+    dropdown.append('<option selected="true" disabled>Choose Region</option>');
+    dropdown.prop('selectedIndex', 0);
+    const url = 'ph-json/region.json';
+    // Populate dropdown with list of regions
+    $.getJSON(url, function (data) {
+        $.each(data, function (key, entry) {
+            dropdown.append($('<option></option>').attr('value', entry.region_code).text(entry.region_name));
+        })
+    });
+
+});
